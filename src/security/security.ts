fe49e9da5122a38b834d86e094c4abf80f97d5d8
Santiago Gonzalez Mota <santiago.gonzalez.mota@emeal.nttdata.com>
2022-03-13 20:31:06 +0100

    feat: add security

--- src/security/security.ts
+++ src/security/security.ts
@@ -1,14 +1,28 @@
+import { getServices, loadEnv, readCFServices } from "@sap/xsenv";
 import { Application } from "express";
 import expressBasicAuth from "express-basic-auth";
+import passport from "passport";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { JWTStrategy } = require("@sap/xssec");
+loadEnv();
+const services = readCFServices();
 
 export class Security {
-  public static setup(app: Application) {
-    const pwd = process.env.ADMIN_PASSWORD || "admin";
-    app.use(
-      expressBasicAuth({
-        challenge: true,
-        users: { admin: pwd },
-      })
-    );
+  public static setup(app: Application, isProduction: boolean) {
+    if (isProduction) {
+      passport.use(
+        new JWTStrategy(getServices({ uaa: { tag: "xsuaa" } }, services).uaa)
+      );
+      app.use(passport.initialize());
+      app.use(passport.authenticate("JWT", { session: false }));
+    } else {
+      const pwd = process.env.ADMIN_PASSWORD || "admin";
+      app.use(
+        expressBasicAuth({
+          challenge: true,
+          users: { admin: pwd },
+        })
+      );
+    }
   }
 }
